7c35f8ec4f8040165d509b4b76589d4466fd0855
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-08 17:04:38 -0300

    Projeto criado

---
+++ src/main/java/com/tadkey/java/web/com/spring/boot/JavaWebComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tadkey.java.web.com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadkey/java/web/com/spring/boot/JavaWebComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadkey.java.web.com.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
