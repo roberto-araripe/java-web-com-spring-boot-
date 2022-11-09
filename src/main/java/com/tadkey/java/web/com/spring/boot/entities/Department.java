ef7de4a77cd436e84206ea32cab50eb9973b5c9f
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-09 09:11:26 -0300

    Criadas as entidades User e Department

---
+++ src/main/java/com/tadkey/java/web/com/spring/boot/entities/Department.java
@@ -0,0 +1,26 @@
+package com.tadkey.java.web.com.spring.boot.entities;
+
+public class Department {
+	
+	private Long id;
+	private String name;
+	
+	public Department() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
