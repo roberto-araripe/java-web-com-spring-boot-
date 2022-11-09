e0e6dd4e247a93716f9eba9d7e332195411c7cc7
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-09 09:32:17 -0300

    Mapeando os objetos relacionais do banco de dados

--- src/main/java/com/tadkey/java/web/com/spring/boot/entities/Department.java
+++ src/main/java/com/tadkey/java/web/com/spring/boot/entities/Department.java
@@ -1,7 +1,17 @@
 package com.tadkey.java.web.com.spring.boot.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
